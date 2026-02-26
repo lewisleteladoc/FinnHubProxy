a30ca11a2a1cf436f3be60b4be530c5b5911415e
Lewis Le <haile819@gmail.com>
2026-02-26 12:07:16 -0800

    make it work in docker desktop

--- Program.cs
+++ Program.cs
@@ -1,6 +1,30 @@
+﻿/*
+ * This version:
+
+Works locally
+
+Works in Docker
+
+Enables Swagger everywhere
+
+Avoids HTTPS issues
+
+Binds correctly inside containers
+*/
+
 using FinnHubProxy.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// 🔑 VERY IMPORTANT for Docker
+var configuration = builder.Configuration;
+
+if (configuration["IS_DOCKER"] == "true")
+{
+    Console.WriteLine(Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER"));
+    builder.WebHost.UseUrls("http://0.0.0.0:8080");
+}
+
 
 // Add services to the container.
 
@@ -23,13 +47,19 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true")
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// DO NOT redirect HTTPS in containers
+var runningInContainer = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
+
+if (!runningInContainer)
+{
+    app.UseHttpsRedirection();
+}
 
 // Enable CORS
 app.UseCors();
@@ -37,5 +67,7 @@ app.UseCors();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/", () => Results.Ok("FinnHubProxy is running"));
 
 app.Run();
